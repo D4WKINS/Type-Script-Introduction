f481e7ac3f79149225432a4625eaca47269868d4
Carl <78683271+D4WKINS@users.noreply.github.com>
2021-08-17 08:52:41 +0000

    Update

---
+++ index.js
@@ -0,0 +1,9 @@
+var greeting = "Hello World!";
+var x;
+x = "Carl";
+console.log(x);
+var user = {};
+//valid TS Data Types: STRING, NUMBER, BOOLEAN, ENY
+var y; //Accepts numbers only
+var z = Array < string > //Accepts strings only
+    z.push(true);
---
+++ index.ts
@@ -0,0 +1,19 @@
+
+
+let greeting = "Hello World!"
+
+let x: string
+x = "Carl"
+
+const user={
+    name:"Carl",
+    country:"England"
+}
+//valid TS Data Types: STRING, NUMBER, BOOLEAN, ENY
+let y: number[] //Accepts numbers only
+let z = Array<string> //Accepts strings only
+
+z.push("Carlos")
+const addition = (n1,n2) =>{
+    return n1 + n2
+}
